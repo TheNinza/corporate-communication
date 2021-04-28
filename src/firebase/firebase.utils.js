daeeec4a0015e0d4ea9919b077cd4df17cf7b04a
Nikhil Kumar Gupta <niks.a3198@gmail.com>
2021-04-28 23:16:35 +0530

    edited firebaseConfig

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,12 +4,12 @@ import "firebase/firestore";
 
 // config data for firebase. This section can be pulic
 const firebaseConfig = {
-  apiKey: "AIzaSyDtyGGyclEqKnfsEyHHi784Hm0ADDQjaSc",
-  authDomain: "corporate-comms.firebaseapp.com",
-  projectId: "corporate-comms",
-  storageBucket: "corporate-comms.appspot.com",
-  messagingSenderId: "393604605042",
-  appId: "1:393604605042:web:b4693c89b77b2b7c460eeb",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialising firebase
